cda65a1bedca47e930bfbbf8ed1a0b7bfa3a8709
AYMEN <moniakamli95@gmail.com>
2021-04-12 16:34:18 +0200

    implementing the game in progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import ColorCard from './components/ColorCard';
+import timeout from './utils/util';
 
 function App() {
   const [isOn, setIsOn] = useState(false);
@@ -9,14 +10,48 @@ function App() {
     isDisplay: false, 
     colors: [],
     score: 0,
-    userState: false,
+    userPlay: false,
     userColors: []
   };
 
   const [play, setPlay] = useState(initPlay);
+  const [flashColor, setFlashColor] = ("");
 
   function startHandle() {
     setIsOn(true);
+  }
+
+  useEffect(() => {
+    if(isOn) {
+      setPlay({...initPlay, isDisplay:true})
+    } else {
+      setPlay(initPlay)
+    }
+  }, [isOn]);
+
+  useEffect(() => {
+    if (isOn && play.isDisplay) {
+      let newColor = colorList[Math.floor(Math.random()*4)]
+
+      const copyColors = [...play.colors];
+      copyColors.push(newColor);
+      setPlay({...play, color:copyColors});
+    }
+  }, [isOn, play.isDisplay])
+
+  useEffect(() => {
+    if(isOn && play.isDisplay && play.colors.length) {
+      displayColors();
+    }
+  }, [isOn, play.isDisplay, play.colors.length]);
+
+  async function displayColors() {
+    for (let i=0; i<play.colors.length; i++) {
+      setFlashColor(play.colors[i]);
+      await timeout(1000);
+      // setFlashColor("");
+      // await timeout(1000);
+    }
   }
    
   return (
@@ -24,10 +59,18 @@ function App() {
       <header className="App-header">
       <div className="card__wrapper">
         {colorList && colorList.map((value) => (
-          <ColorCard color={value}/>
+          <ColorCard flash={flashColor === value} color={value}/>
         ))}
       </div>
-      <button onClick={startHandle} className="start__button">Start</button>
+      { !isOn && !play.score && (
+        <button onClick={startHandle} className="start__button">
+          Start
+          </button>
+      )}
+
+      { isOn && (play.isDisplay || play.userPlay) && (
+       <div className="score">{play.score}</div>
+      )}
       </header>
     </div>
   );
